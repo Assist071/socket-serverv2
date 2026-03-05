b295ca99d8519a5713cd112e4703a55c9f0d24fa
Brenthings <ocdamia14@gmail.com>
2026-03-06 00:05:18 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
+
+// Example event: broadcast new order
+io.on("connection", (socket) => {
+  console.log("Client connected:", socket.id);
+
+  socket.on("new-order", (orderData) => {
+    io.emit("order-notification", orderData); // broadcast to all clients
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected:", socket.id);
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Socket.IO server running on port ${PORT}`);
+});
