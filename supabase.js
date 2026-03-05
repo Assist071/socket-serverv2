fa3c175b11f41b54f99941621e2d33de59c37c4b
Brenthings <ocdamia14@gmail.com>
2026-03-06 04:09:19 +0800

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,8 +1,1 @@
-const { createClient } = require('@supabase/supabase-js');
 
-const supabase = createClient(
-  process.env.SUPABASE_URL || 'https://pljfczpwersfytcxbwvk.supabase.co',
-  process.env.SUPABASE_SERVICE_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBsamZjenB3ZXJzZnl0Y3hid3ZrIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3MTQ1MDcxMywiZXhwIjoyMDg3MDI2NzEzfQ.k0UYHuLMFpSbr7-rkHCiAfzzBwRMz9hXwKhA-obSj9Q'
-);
-
-module.exports = supabase;
